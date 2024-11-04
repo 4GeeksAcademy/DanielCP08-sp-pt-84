6db61b3321d64176e52bfb499a8935c48864103c
Daniel Campón Perdigones <campondaniel@gmail.com>
2024-11-04 21:05:18 +0000

    newProject[Primera version]

--- src/app.js
+++ src/app.js
@@ -4,8 +4,50 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { left } from "@popperjs/core";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let suits = ["♦", "♥", "♠", "♣"];
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let card = document.getElementById("card");
+  let suitTop = document.getElementById("suit-top");
+  let numberCard = document.getElementById("number");
+  let suitBot = document.getElementById("suit-bot");
+
+  suitTop.style.textAlign = "left";
+  suitTop.style.fontSize = "90px";
+
+  suitBot.style.textAlign = "left";
+  suitBot.style.rotate = "180deg";
+  suitBot.style.fontSize = "90px";
+
+  numberCard.style.fontSize = "126px";
+
+  let randomSuit = Math.floor(Math.random() * 4);
+  let randomNumber = Math.floor(Math.random() * 13);
+
+  numberCard.innerHTML = numbers[randomNumber];
+
+  suitTop.innerHTML = suits[randomSuit];
+  suitBot.innerHTML = suits[randomSuit];
+  if (suits[randomSuit] == "♥" || suits[randomSuit] == "♦") {
+    suitTop.style.color = "red";
+    suitBot.style.color = "red";
+  }
 };
